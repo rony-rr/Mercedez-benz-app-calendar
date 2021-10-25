2522337832ed5e80a5b18efcd5f369fd7054f728
rony-dive-rr <javieralexander4558@gmail.com>
2021-10-24 18:47:07 -0600

    se agrego hook useFonts

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react'
+import { View, Text } from 'react-native'
+import useFont from "./src/utils/useFonts/index"
 
-export default function App() {
+const Root = () => {
+  const [authCheck, setAuthCheck] = useState(null)
+  const [loadingFonts, setLoadingFonts] = useState(null);
+
+  useFont.loadFonts().then(res=>setLoadingFonts(res));
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Welcome</Text>
-      <StatusBar style="auto" />
+    <View style={{alignItems:'center',justifyContent:'center',flex:1}}>
+      <Text>Cargando fuente</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Root
--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const navigation = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
-export default navigation
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
