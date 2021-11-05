99358ea8dd5955b4c932d4b5236dfe78eaf23a91
Javier Alexander <javieralexander4558@gmail.com>
2021-11-05 15:10:48 -0600

    update login

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
 import Login from "./view";
 import fetchHook from "../../utils/useFetch";
 import GlobalVars from "../../global/globalVars";
 import storage from '../../utils/useLocalStorage'
-import Alert from '../../utils/useAlert'
-=======
-import { View, Text } from "react-native";
-import Login from "./view";
-import fetchHook from "../../utils/useFetch";
-import GlobalVars from "../../global/globalVars";
+import Alert from '../../utils/useAlert';
 
->>>>>>> 71648aaca1b9fd91895ef6a649e30b20229f4455
 const index = ({ navigation }) => {
   const [openModal, setOpenModal] = useState(false);
 
@@ -21,16 +14,10 @@ const index = ({ navigation }) => {
     setOpenModal(true);
     try {
       const response = await fetchHook.fetchPost(urlLogin, data);
-<<<<<<< HEAD
       if (response.status == true) {
         storage.storeData('userToken',response.token);
         setOpenModal(false);
         navigation.navigate("TabNavigation");
-=======
-      if (response.status) {
-        navigation.navigate("TabNavigation");
-        setOpenModal(false);
->>>>>>> 71648aaca1b9fd91895ef6a649e30b20229f4455
       } else {
         setOpenModal(false);
         Alert("Mensaje", "Datos incorrectos");
